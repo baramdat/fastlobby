c904e7209620a21a4f0e590570e20828211f0a24
baramdat <baramdat@gmail.com>
2023-01-04 11:26:08 +0500

    appointment page current previous url issue

--- resources/views/templates/appointment/register_visitor_detail.blade.php
+++ resources/views/templates/appointment/register_visitor_detail.blade.php
@@ -158,7 +158,7 @@
                                     @else
                                         <div class="text-center">
                                             <p><strong class="text-danger"> This appointment has already been checked!</strong></p>
-                                            {{url()->current()}} {{url()->previous()}}
+                                            <!-- {{url()->current()}} {{url()->previous()}} -->
                                             @if(url()->current() != url()->previous())
                                             <a href="{{url()->previous()}}" class="btn btn-success mt-2">
                                                 Go Back
